287c21f6b4e9d9321bc2523a6d34148a0581171e
AdrienEHB <Adrien.schautteet@student.ehb.be>
2014-12-10 12:33:01 +0100

    CameraFollow to C# file and starting with enemies
    Converted the ComeraFollow script from js to c# and started working on
    enemies in space: inbound comets

---
+++ Assets/Working Assets/Code/Inbound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inbound : MonoBehaviour {
+
+	public GameObject inboundComet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+		
+	}
+}
