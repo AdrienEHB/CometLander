9ae7bba8acae96e136d57386b20da45855f6e17c
AdrienEHB <Adrien.schautteet@student.ehb.be>
2014-12-10 11:07:18 +0100

    Initial Commit
    Initial commit for teammate usage

---
+++ Assets/Working Assets/Code/Ship.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	public float thrustpower = 20;
+	public float thrustrotation = 3;
+	public Transform gravityCenter;
+	public float gravity = 1;
+	public Camera cam;
+	public float camSensitivity = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void Update(){
+		//cam.orthographicSize = 15 + rigidbody2D.velocity.magnitude * camSensitivity;
+		cam.orthographicSize = Mathf.Lerp(cam.orthographicSize,15 + rigidbody2D.velocity.magnitude * camSensitivity,Time.deltaTime * 2);
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey("left")){
+			this.rigidbody2D.AddRelativeForce(new Vector2(0, thrustpower));
+			this.rigidbody2D.AddTorque(-thrustrotation);
+			
+		}
+		if(Input.GetKey("right")){
+			this.rigidbody2D.AddRelativeForce(new Vector2(0, thrustpower));
+			this.rigidbody2D.AddTorque(thrustrotation);
+		}
+		
+		/*if (transform.position.x > gravityCenter.position.x){
+			rigidbody2D.AddForce(new Vector2(-gravity,0));
+		} else {
+			rigidbody2D.AddForce(new Vector2(gravity,0));
+		}
+		
+   		if (transform.position.y > gravityCenter.position.y){
+			rigidbody2D.AddForce(new Vector2(0,-gravity));
+		} else {
+			rigidbody2D.AddForce(new Vector2(0,gravity));
+   		}*/
+		
+		Vector2 gravityForce = transform.position - gravityCenter.position;
+		rigidbody2D.AddForce(-gravityForce * gravity);
+		
+	}
+}
